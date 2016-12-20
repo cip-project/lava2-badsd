710d5d51da42cd7794fae1264eb1d37c0aabae49
Don Brown <don.f.brown@comcast.net>
2016-12-19 22:58:38 -0500

    change from apt to apt-get per error message

--- integration-scripts/install_lava.sh
+++ integration-scripts/install_lava.sh
@@ -9,8 +9,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy update
 # LAVA is currently packaged for Debian unstable using Django1.8 and Postgresql. 
 # LAVA packages are now available from official Debian mirrors for Debian 
 # unstable. e.g. to install the master, use:
-sudo DEBIAN_FRONTEND=noninteractive apt install postgresql
-sudo DEBIAN_FRONTEND=noninteractive apt -t jessie-backports install lava-server
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install postgresql
+sudo DEBIAN_FRONTEND=noninteractive apt-get -t jessie-backports -y install lava-server
 sudo DEBIAN_FRONTEND=noninteractive a2dissite 000-default
 sudo DEBIAN_FRONTEND=noninteractive a2enmod proxy
 sudo DEBIAN_FRONTEND=noninteractive a2enmod proxy_http
