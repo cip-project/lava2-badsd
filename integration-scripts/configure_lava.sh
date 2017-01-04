aa20a72f7c3c97fc684ba3711c8ce6d300b0d70d
Don Brown <don.f.brown@comcast.net>
2017-01-04 18:45:47 -0500

    Add device types and devices

--- integration-scripts/configure_lava.sh
+++ integration-scripts/configure_lava.sh
@@ -3,11 +3,34 @@
 # Add Lava Server Superuser
 sudo DEBIAN_FRONTEND=noninteractive lava-server manage createsuperuser --username lavauser --email=lavauser@codethink.co.uk
 
-# Add all existing Device Types to Lava Server
+# QEMU
+# Add a Device Type qemu and Device qemu01 for the QEMU VM
 cd /etc/lava-server/dispatcher-config/device-types/
-sudo DEBIAN_FRONTEND=noninteractive lava-server manage add-device-type '*'
+sudo DEBIAN_FRONTEND=noninteractive lava-server manage add-device-type qemu
+sudo DEBIAN_FRONTEND=noninteractive lava-server manage add-device --device-type qemu qemu01
 
-#Return back to home directory
+# Create the Device Dictionary file for a QEMU VM and store it in ~/myqemu.dat
 cd ~
+echo "{% extends 'qemu.jinja2' %}" > myqemu.dat 
+echo "{% set mac_addr = '52:54:00:12:34:59' %}" >> myqemu.dat
+echo "{% set memory = '1024' %}" >> myqemu.dat
+
+# Import the QEMU Device Dictionary file into the LAVA2 Server
+sudo DEBIAN_FRONTEND=noninteractive lava-server manage device-dictionary --hostname qemu01 --import myqemu.dat 
+
+# Beaglebone Black
+# Add a Device Type beaglebone-black and Device bbb01 for the Beaglebone Black
+cd /etc/lava-server/dispatcher-config/device-types/
+sudo DEBIAN_FRONTEND=noninteractive lava-server manage add-device-type beaglebone-black
+sudo DEBIAN_FRONTEND=noninteractive lava-server manage add-device --device-type beaglebone-black bbb01
+
+# Create the Device Dictionary file for the Beaglebone Black and store it in ~/mybbb.dat
+cd ~
+echo "{% extends 'beaglebone-black.jinja2' %}" > mybbb.dat
+echo "{% set ssh_host = '172.16.200.165' %}" >> mybbb.dat
+echo "{% set connection_command = 'telnet localhost 6000' %}" >> mybbb.dat
+
+# Import the Beaglebone Black Device Dictionary file into the LAVA2 Server
+sudo lava-server manage device-dictionary --hostname bbb01 --import mybbb.dat
 
 
--- integration-scripts/install_lava.sh
+++ integration-scripts/install_lava.sh
@@ -9,8 +9,20 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy update
 # LAVA is currently packaged for Debian unstable using Django1.8 and Postgresql. 
 # LAVA packages are now available from official Debian mirrors for Debian 
 # unstable. e.g. to install the master, use:
+
+# Install postgresql
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install postgresql
+
+# Install qemu & KVM
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install qemu-kvm libvirt-bin
+
+# Add teh vagrant user to the libvirtd and kvm groups
+sudo DEBIAN_FRONTEND=noninteractive usermod -a -G libvirtd,kvm vagrant
+
+# Install Lava Server
 sudo DEBIAN_FRONTEND=noninteractive apt-get -t jessie-backports -y install lava-server
+
+# Configure Apache web server & restart the apache2 service
 sudo DEBIAN_FRONTEND=noninteractive a2dissite 000-default
 sudo DEBIAN_FRONTEND=noninteractive a2enmod proxy
 sudo DEBIAN_FRONTEND=noninteractive a2enmod proxy_http
